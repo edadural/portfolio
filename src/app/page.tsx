4fd87e1d34f4d14e3ea885573d9960e32db5cba6
Eda Dural <edaadurall@gmail.com>
2025-07-12 07:34:47 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/pages/Header";
+import Hero from "@/components/pages/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+}
